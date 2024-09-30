ef3002df86815c9ebb0c96cc9f1903dd6e942138
Francisco Antonio da Luz Filho <falf@cesar.school>
2024-09-30 09:23:59 -0300

    fix: nomes das classes

---
+++ Pet-Lucky/dominio/adocao/src/main/java/lp/adocao/dominio/abrigo/IAbrigoRepositorio.java
@@ -0,0 +1,11 @@
+package lp.adocao.dominio.abrigo;
+
+import java.util.List;
+
+public interface IAbrigoRepositorio {
+    void salvar(Abrigo abrigo);
+    void editar(Abrigo abrigo);
+    void remover(IdAbrigo idAbrigo);
+    Abrigo obterPorId(IdAbrigo idAbrigo);
+    List<Abrigo> listarAbrigos();
+}

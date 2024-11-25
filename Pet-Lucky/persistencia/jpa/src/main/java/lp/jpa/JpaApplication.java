3ad32b810d9fd6667d50fb4068b94bc91ce255da
Francisco Antonio da Luz Filho <falf@cesar.school>
2024-11-25 20:24:07 -0300

    Update JpaApplication.java

--- Pet-Lucky/persistencia/jpa/src/main/java/lp/jpa/JpaApplication.java
+++ Pet-Lucky/persistencia/jpa/src/main/java/lp/jpa/JpaApplication.java
@@ -1,13 +1,23 @@
 package lp.jpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("Aplicação está rodando... Pressione Ctrl+C para sair.");
+            Thread.currentThread().join(); // Aguarda até que a aplicação seja interrompida
+        };
     }
 
 }
